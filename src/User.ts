804b392e62f677fbdcb87bc545030fd01307ee77
jonashladik <jonhlad@gmail.com>
2025-04-06 02:23:52 +0200

    feat: add some props to user

--- src/User.ts
+++ src/User.ts
@@ -3,6 +3,22 @@ import { APIUser } from "discord-api-types/v10";
 export default class User {
   private data: APIUser;
 
+  get id() {
+    return this.data.id;
+  }
+
+  get username() {
+    return this.data.username;
+  }
+
+  get discriminator() {
+    return this.data.discriminator;
+  }
+
+  get globalName() {
+    return this.data.global_name;
+  }
+
   constructor(data: APIUser) {
     this.data = data;
   }
