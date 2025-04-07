5cbb28ae901cba3242e1fa48099a43d47f1d84b5
jonashladik <jonhlad@gmail.com>
2025-04-07 17:10:37 +0200

    feat: add headers to API fn

--- src/DiscordAPI.ts
+++ src/DiscordAPI.ts
@@ -1,7 +1,8 @@
 export default async function discordAPIRequest<Ret = undefined>(
   endpoint: string,
   method: "GET" | "POST" | "PUT" | "DELETE" | "PATCH",
-  body?: Extract<"POST" | "PUT", typeof method> extends never ? never : object
+  body?: Extract<"POST" | "PUT", typeof method> extends never ? never : object,
+  headers?: Record<string, string>
 ): Promise<Ret> {
   const req = await fetch(`https://discord.com/api/v10/${endpoint}`, {
     method,
@@ -9,6 +10,7 @@ export default async function discordAPIRequest<Ret = undefined>(
       "User-Agent": "DiscordBot (example.com, v0.1)",
       "Content-Type": "application/json",
       Authorization: `Bot ${process.env.DISCORD_TOKEN}`,
+      ...headers,
     },
     body: body ? JSON.stringify(body) : undefined,
   });
